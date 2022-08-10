8ba031000c741b7fd277dbf936b672e6c5b9b296
Jennifer Herdzina <92278996+jherdzina@users.noreply.github.com>
2022-08-09 21:46:45 -0500

    heroku deployment

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5001;
 const URI = process.env.MONGO_URI;
 
 console.log(URI)
-mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`${URI}`, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
